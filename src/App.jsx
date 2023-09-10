cd7721dfe07da4abeab06f993d4899114d065123
Chris Vaz <chrisvaz17@gmail.com>
2023-09-10 16:33:23 -0400

    fixed pathing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "../styles/App.css";
 import axios from "axios";
 import Calendar from "react-widgets/Calendar";
 import "react-widgets/styles.css";
