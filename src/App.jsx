64e2827d9e4d8d3e71b90d28e010ae9ed8e40592
Emma Clarke <emmacodeswell@gmail.com>
2023-09-24 16:28:29 -0400

    Initialize Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,18 @@ import Footer from "./components/Footer";
 import formatTime from "./helpers/formatTime";
 import getUserTimezoneDate from "./helpers/getUserTimezoneDate";
 import { useAccessors } from "react-widgets/cjs/Accessors";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_tBx3qX1mmQzbKhq0dZondFz4HTMjQK0",
+  authDomain: "sun-run-feb5f.firebaseapp.com",
+  projectId: "sun-run-feb5f",
+  storageBucket: "sun-run-feb5f.appspot.com",
+  messagingSenderId: "383995255223",
+  appId: "1:383995255223:web:7879e1bbb6889e9f7ff032"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function AppRouter() {
   const router = createBrowserRouter([
