30a5dd82526f7f32b5c6f1a0c75fcad831124b8f
Chris Vaz <chrisvaz17@gmail.com>
2023-10-03 16:39:35 -0400

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   Link,
   useLocation,
 } from "react-router-dom";
-import "../styles/App.css";
+import "./App.scss";
 import "react-widgets/styles.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
